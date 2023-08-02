1b108f2f8ecef2be4ed9bfa5f0ae3e3990956dbf
Amanda Lee <amanda.lee.22@ucl.ac.uk>
2023-08-02 01:35:09 +0100

    Deploy gh-pages

---
+++ _next/static/4Rh40gkJ3pKnpZL8_wRpN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cc8f0cfa-d8c4f2716060e29e.js","static/css/d92f229295992ef1.css","static/chunks/pages/index-1c0a2c770f2f4cff.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
